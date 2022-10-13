84156f9f56076bd775dc5703a0fb9d210d36a632
VladKharchenko29 <vlad.mego@icloud.com>
2022-10-13 11:22:37 +0200

    fix

--- cypress/support/Pages/HomePage.js
+++ cypress/support/Pages/HomePage.js
@@ -4,7 +4,7 @@ const data = {
     userTab: ".css-h6m406",
     userList: ".css-ji22ry > li",
     plansTab: ".sm\:text-sm li:nth-of-type(1)",
-    premiumPlan: ".border-gray-300 > .w-full"
+    premiumPlan: '.border-gray-300 > .w-full'
 }
 
 class HomePage {
@@ -15,6 +15,7 @@ class HomePage {
     }
 
     selectPremiumPlan() {
+        cy.wait(2000)
         cy.contains('Тарифы').click()
         cy.get(data.premiumPlan).should('be.visible').click()
         cy.contains('Оформление подписки Premium').should('be.visible')
